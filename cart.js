1a96bfa8bf4c632a957f1fedd08a6af77e0f5c96
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2021-11-14 15:24:55 +0530

    cart page js

--- cart.js
+++ cart.js
@@ -1,43 +1,99 @@
+i
 var prodInfo = JSON.parse(localStorage.getItem("Dermcart"))||[]
-
-function displayitems(prodInfo) {
-    document.querySelector("tbody").textContent = "";
-
-    prodInfo.map(function (item, index) {
-        var tr = document.createElement("tr");
-        tr.setAttribute("id", "setfontsize")
-
-        var td1 = document.createElement("td");
-        td1.textContent = item.image_url;
-
-        var td2 = document.createElement("td");
-        td2.textContent =item.MRP ;
-
-        var td3 = document.createElement("td");
-        td3.textContent = counter;
-
-        var td4 = document.createElement("td");
-        td4.textContent = item.MRP * counter;
-
-        var td5 = document.createElement("td");
-        td5.textContent = "delete";
-        td5.addEventListener("click", deleteItem);
+displayitems(prodInfo)
+console.log(prodInfo)
 
 
+ function displayitems(prodInfo) {
+     document.querySelector(".cart-items").textContent = "";
 
-        tr.append(td1, td2, td3, td4, td5);
-        document.querySelector("tbody").append(tr);
+     prodInfo.map(function (item, index) {
+        var cartRow = document.createElement('div')
+        cartRow.classList.add('cart-row')  
+
+        var cartItem = document.createElement("div");
+        cartItem.classList.add('cart-item ')
+        cartItem.textContent = item.image_url;
+
+         var cartPrice = document.createElement("div");
+         cartPrice.classList.add('cart-price')
+         cartPrice.textContent =item.MRP ;
+
+       var quantity = document.createElement("input");
+       quantity.classList.add('cart-quantity')
+         quantity.innerHTML='<input type="number></input>' ;
+
+       var SubPrice = document.createElement("td");
+       SubPrice.textContent = item.MRP * quantity.innerHTML;
+
+         var remove = document.createElement("div");
+        remove.textContent = "delete";
+        remove.addEventListener("click", deleteItem);
+
+        cartRow.append(cartItem, cartPrice, quantity, SubPrice, remove);
+        document.querySelector("cart-items").append(cartRow);
     });
 
 
-}
-window.addEventListener("load", function () {
-    var productInfo = JSON.parse(localStorage.getItem("Dermcart"));
-    displayitems(productInfo)
+ }
+ window.addEventListener("load", function () {
+    var prodInfo = JSON.parse(localStorage.getItem("Dermcart"));
+   displayitems(prodInfo)
 })
 
 
 function deleteItem(event) {
-  console.log(event.target);
+   console.log(event.target);
   event.target.parentNode.remove();
+}
+
+
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+//
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
+
+function check(e) {
+  let inp = document.getElementById("input12");
+
+  let code = inp.promo.value;
+
+
+  if(code==="masai30"){
+      //.textContent= Math.floor(mon.textContent * 0.7);
+  } else {
+      alert("Please Enter Valid Promo Code")
+  }
+  let pro = document.getElementById("promo");
+  inp.promo.value="";
+}
+
+function paymentPage() {
+  window.location.href ="payment.html"
+}
+function checkOut() {
+  window.location.href ="success.html"
 }
