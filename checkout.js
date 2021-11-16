5b665527e42af8d4a4c1b5a6d87c9ec8fff969c7
MUKESH KUMAR <66511917+Mukesh16407@users.noreply.github.com>
2021-11-16 17:38:10 +0530

    some  changes in checkout page

--- checkout.js
+++ checkout.js
@@ -1,1 +1,109 @@
+
+document.getElementById("formid").addEventListener("submit",formValidation);
+
+
+var Email = document.getElementById("email").value;
+var Name = document.getElementById("name").value;
+var Phone = document.getElementById("phone").value;
+
 document.getElementById("myText").defaultValue = "United State";
+ console.log(Email)
+
+function formValidation(e) {
+  e.preventDefault();
+
+  var gestObj ={
+      Email:formid.email.value,
+      Name:formid.name.value,
+      phone:formid.phone.value
+  };
+  
+   console.log(gestObj)
+  
+  
+ if(Email== "" ){
+ 
+  document.getElementById("email").style.borderColor = "red";
+  document.getElementById("WrongEmail").textContent =
+    "Email address is a required field";
+  document.getElementById("WrongEmail").style.color =
+   "rgb(250,232,230)";
+   return false
+}  
+if(Email.indexOf('@') <= 0 ){
+ 
+  document.getElementById("email").style.borderColor = "red";
+  document.getElementById("WrongEmail").textContent =
+    "** @ invalid Postion";
+  document.getElementById("WrongEmail").style.color =
+   "rgb(250,232,230)";
+   return false
+}  
+if((Email.charAt(Email.length -4) != ".") && (Email.charAt(Email.length -3) != ".")){
+ 
+  document.getElementById("email").style.borderColor = "red";
+  document.getElementById("email2").textContent =
+    "** @ invalid Postion";
+  document.getElementById("email2").style.color =
+   "rgb(250,232,230)";
+   return false
+} 
+if(Name == ""){
+        
+    document.getElementById("name").style.borderColor = "red";
+    document.getElementById('WrongName').textContent = '**Full Name is a required field';
+    document.getElementById('WrongName').style.color="rgb(250,232,230)";
+    return false
+}
+  
+if(Name.length <= 2){
+  document.getElementById("name").style.borderColor = "red";
+  document.getElementById('WrongName').textContent = '**Please Fill Valid User Name';
+  document.getElementById('WrongName').style.color="rgb(250,232,230)";
+  return false
+}
+
+if(!isNaN(Name)){
+  document.getElementById("name").style.borderColor = "red";
+  document.getElementById('WrongName').textContent = '**Only Characters are allowed';
+  document.getElementById('WrongName').style.color="rgb(250,232,230)";
+  return false
+} 
+
+
+
+
+  
+  
+   if(Phone == ""){
+    document.getElementById("phone").style.borderColor = "red";
+    document.getElementById('wrongPhone').textContent = '**phone Number is Required';
+    document.getElementById('wrongPhone').style.color="rgb(250,232,230)";
+    return false
+   }
+   
+   if(isNaN(Phone)){
+    Phone.value ="";
+    return false
+   }
+   if(Phone.length < 10){
+    document.getElementById("phone").style.borderColor = "red";
+    document.getElementById('wrongPhone').textContent = '**Please Enter 10 Digts Number';
+    document.getElementById('wrongPhone').style.color="rgb(250,232,230)";
+    return false
+
+  }
+  if(Phone.length > 10){
+    document.getElementById("phone").style.borderColor = "red";
+    document.getElementById('wrongPhone').textContent = '**Please Enter 10 Digts Number';
+    document.getElementById('wrongPhone').style.color="rgb(250,232,230)";
+    return false;
+  } else{
+      
+       alert("Thank you For Share Your Details");
+       document.getElementById("detailsCheck").addEventListener("click", function () {
+        window.location.href ="product.html"
+      });
+   }
+  //console.log(arrObj);  
+}
--- signup.js
+++ signup.js
@@ -22,27 +22,81 @@ function signUp() {
      referral:referral
     };
     console.log(DreamStoreuser)
-   if(user.length === 0 || phone.length === 0 || pass.length < 6 || email.length == 0  || email.value != email1.value || pass.value != pass1.value){
-    document.getElementById("user").style.borderColor = "red";
-    document.getElementById('user1').textContent = 'Full Name is a required field';
-    document.getElementById('user1').style.backgroundColor="rgb(250,232,230)";
+    
+    if(user == ""){
+        
+        document.getElementById("user").style.borderColor = "red";
+        document.getElementById('user1').textContent = '**Full Name is a required field';
+        document.getElementById('user1').style.backgroundColor="rgb(250,232,230)";
+        return false
+    }
+      
+    if(user.length <= 2){
+      document.getElementById("user").style.borderColor = "red";
+      document.getElementById('user1').textContent = '**Please Fill Valid User Name';
+      document.getElementById('user1').style.backgroundColor="rgb(250,232,230)";
+      return false
+    }
 
-    document.getElementById("email").style.borderColor = "red";
-    document.getElementById('email2').textContent = 'Email address is a required field';
-    document.getElementById("email2").style.backgroundColor = "rgb(250,232,230)";
+    if(!isNaN(user)){
+      document.getElementById("user").style.borderColor = "red";
+      document.getElementById('user1').textContent = '**Only Characters are allowed';
+      document.getElementById('user1').style.backgroundColor="rgb(250,232,230)";
+      return false
+    }
+     if(email== "" ){
+     
+      document.getElementById("email").style.borderColor = "red";
+      document.getElementById("email2").textContent =
+        "Email address is a required field";
+      document.getElementById("email2").style.backgroundColor =
+       "rgb(250,232,230)";
+       return false
+    }  
+    if(email.indexOf('@') <= 0 ){
+     
+      document.getElementById("email").style.borderColor = "red";
+      document.getElementById("email2").textContent =
+        "** @ invalid Postion";
+      document.getElementById("email2").style.backgroundColor =
+       "rgb(250,232,230)";
+       return false
+    }  
+    if((email.charAt(email.length -4) != ".") && (email.charAt(email.length -3) != ".")){
+     
+      document.getElementById("email").style.borderColor = "red";
+      document.getElementById("email2").textContent =
+        "** @ invalid Postion";
+      document.getElementById("email2").style.backgroundColor =
+       "rgb(250,232,230)";
+       return false
+    }  
 
-    document.getElementById("pass").style.borderColor = "red";
-    document.getElementById('email3').textContent = 'Please Fill Same Email';
-    document.getElementById("email3").style.backgroundColor = "rgb(250,232,230)";
+     if( email != email1 ){
+    
+      document.getElementById('email3').textContent = 'Please Fill Same Email';
+       document.getElementById("email3").style.backgroundColor = "rgb(250,232,230)";
+     }
+     if(pass.length <= 5 || pass == "" ){
+     
+       document.getElementById("pass").style.borderColor = "red";
+         return false
+     }
 
-    document.getElementById('pass3').textContent = 'Please Fill Same Password';
-    document.getElementById("pass3").style.backgroundColor = "rgb(250,232,230)";
-   
-   }
+    if(pass != pass1){
+      
+       document.getElementById('pass3').textContent = 'Please Fill Same Password';
+       document.getElementById("pass3").style.backgroundColor = "rgb(250,232,230)";
+       return false
+    }
    else{
+     alert("sucessfully register")
         userArr.push(DreamStoreuser);
         localStorage.setItem("DreamUser", JSON.stringify(userArr))
         window.location.href ="login.html"
    }
  // redirect the user to signin page
 }
+
+
+
